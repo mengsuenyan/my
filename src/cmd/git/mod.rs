7bea9b15d02ecb47d22c42911855b0acecdd4579
mengsuenyan <mengsuenyan@163.com>
2024-05-02 08:56:26 +0800

    my git can update repos from config file

---
+++ src/cmd/git/mod.rs
@@ -0,0 +1,13 @@
+use std::{cell::Cell, path::PathBuf};
+
+pub struct GitCmd {
+    cur_dir: PathBuf,
+    // 每次clone, update后的暂停的最大时间
+    sleep: Cell<u64>,
+    config_path: PathBuf,
+    config_backup_path: PathBuf,
+}
+
+mod cmd;
+
+mod update;
