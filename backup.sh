edb0ad3213c9b2c6ab3ae9b99a1565d00ea51fdb
mengsuenyan <mengsuenyan@163.com>
2024-06-23 21:21:47 +0800

    delete unused file

--- backup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-my sky ~/msy/my -t ~/git/my -x ^target$ -x ^.git$ -x ^.gitignore$ -r --cpus 6 --is-b64
-#my sky ~/git/my -t ~/git/my -x ^target$ -x ^.git$ -x ^.gitignore$ -r --cpus 6 -d --is-b64
